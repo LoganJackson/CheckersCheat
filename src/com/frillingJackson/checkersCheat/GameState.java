2fd9a704ad6573dd2560698bb469b7f3fc36a742
Logan Jackson <loganjackson95@gmail.com>
2014-03-25 15:21:30 -0400

    created commented out code to build a dummy game state
    also changed the constructor to mark unreachable spaces with an ‘X’

--- src/com/frillingJackson/checkersCheat/GameState.java
+++ src/com/frillingJackson/checkersCheat/GameState.java
@@ -6,21 +6,66 @@ public class GameState {
 	//b (brown)= pawn for the dark pieces 
 	//G (green)= king for light piece
 	//t (tan)  = pawn for light pieces
+	//E (empty) = empty space on board
+	//X  = unreachable space
+	
+	//for (int row = 0; row <=7; row++) {
+		//for every row, mark only the "dark" spaces
+	//	if(row%2 == 0){ // if the row is even 
+	//		for (int col = 0; col <= 6; col = col+2) {
+	//			board[row][col] = 'E'; //setting each spot to E or empty.
+	//          board[row][col+1] ='X'; //setting the 
+	//		} 
+	//	}else{ //row is odd
+	//		for (int col = 0; col <= 7; col =col+2) {
+	//          board[row][col] ='X'; //setting the
+	//			board[row][col+1] = 'E'; //setting each spot to E or empty.
+	//		}
+	//	}
+	//}
+	
+	//dummy gamestate
+	
+	//board[0][1] = 'b';
+	//board[0][3] = 'b';
+	//board[0][5] = 'b';
+	//board[0][7] = 'b';
+	
+	//board[1][0] = 'b';
+	//board[1][2] = 'b';
+	//board[1][4] = 'b';
+	//board[1][6] = 'b';
+	
+	//board[2][3] = 'b';
+	
+	//board[7][0] = 't';
+	//board[7][2] = 't';
+	//board[7][4] = 't';
+	//board[7][6] = 't';
+	
+	//board[6][1] = 't';
+	//board[6][3] = 't';
+	//board[6][5] = 't';
+	//board[6][7] = 't';
+	
+	//board[5][4] = 't';
 	
 	GameState(char[][] state){
 		board = state;
 	}
 	
 	GameState(){
-		for (int row = 0; row < 8; row++) {
+		for (int row = 0; row <= 7; row++) {
 			//for every row, mark only the "dark" spaces
 			if(row%2 == 0){ // if the row is even 
-				for (int col = 0; col < 7; col = col+2) {
+				for (int col = 0; col <= 6; col = col+2) {
 					board[row][col] = 'E'; //setting each spot to E or empty.
+					board[row][col+1] ='X'; //setting the
 				} 
 			}else{ //row is odd
-				for (int col = 1; col < 8; col =col+2) {
-					board[row][col] = 'E'; //setting each spot to E or empty.
+				for (int col = 0; col <= 7; col =col+2) {
+					board[row][col] ='X'; //setting the
+					board[row][col+1] = 'E'; //setting each spot to E or empty.
 				}
 			}
 		}
