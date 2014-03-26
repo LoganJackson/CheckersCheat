8f261a2cdfbcd5187ca4c8dfdfcca5e6889e4947
bradfrilling <s14.bfrilling@wittenberg.edu>
2014-03-25 23:07:01 -0400

    Created a dummy state to use for corrections
    Setup a dummy state to use for corrections.
    Working on setting up a onTouchListener to change color of piece to
    correct color.

--- src/com/frillingJackson/checkersCheat/CorrectionActivity.java
+++ src/com/frillingJackson/checkersCheat/CorrectionActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ImageView;
 
 public class CorrectionActivity extends Activity {
@@ -32,5 +33,65 @@ public class CorrectionActivity extends Activity {
 		getMenuInflater().inflate(R.menu.correction, menu);
 		return true;
 	}
+	
+	public GameState correctBoard(GameState old){
+	
+		
+	GameState board = new GameState();
+	
+	//dummy gamestate
+	
+	board.set(0,1,'b');
+	board.set(0,3,'b');
+	board.set(0,5,'b');
+	board.set(0,7,'b');
+	
+	board.set(1,0,'b');
+	board.set(1,2,'b');
+	board.set(1,4,'b');
+	board.set(1,6,'b');
+	
+	board.set(2,3,'b');
+	
+	board.set(7,0,'t');
+	board.set(7,2,'t');
+	board.set(7,4,'t');
+	board.set(7,6,'t');
+	
+	board.set(6,1,'t');
+	board.set(6,3,'t');
+	board.set(6,5,'t');
+	board.set(6,7,'t');
+	
+	board.set(5,4,'t');
+	
+	
+	/*correctionView.setOnTouchListener(new View.OnTouchListener()
+	{
+		public boolean onTouch(){
+			return true;
+		
+	}
+		/*	
+			public boolean onContextItemSelected(MenuItem item) {
+			    
+			   switch (item.getItemId()) {
+			        case R.id.edit:
+			            editNote(info.id);
+			            return true;
+			        case R.id.delete:
+			            deleteNote(info.id);
+			            return true;
+			        default:
+			            return super.onContextItemSelected(item);
+			    }
+			}
+			
+		}
+	  */  
+	
+	
+	return old;
 
+}
 }
