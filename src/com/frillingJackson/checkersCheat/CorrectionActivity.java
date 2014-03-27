154b65c76969353d91095f7f0f9f48480f6a7a7e
bradfrilling <s14.bfrilling@wittenberg.edu>
2014-03-27 11:04:59 -0400

    Added code to represent the pieces
    This code will take a gamestate and represent the pieces on the
    gameboard using the canvas class

--- src/com/frillingJackson/checkersCheat/CorrectionActivity.java
+++ src/com/frillingJackson/checkersCheat/CorrectionActivity.java
@@ -28,6 +28,45 @@ public class CorrectionActivity extends Activity {
 		//}
 		//correctionView = (ImageView)findViewById(R.id.correctionView);
 		//correctionView.setImageBitmap(photo);
+		
+		//P (pink) = king for the dark pieces 
+		//b (brown)= pawn for the dark pieces 
+		//G (green)= king for light piece
+		//t (tan)  = pawn for light pieces
+		//E (empty) = empty space on board
+		//X  = unreachable space
+		
+		GameState board = new GameState();
+		
+		//dummy gamestate
+		
+		board.set(0,1,'b');
+		board.set(0,3,'b');
+		board.set(0,5,'b');
+		board.set(0,7,'b');
+		
+		board.set(1,0,'b');
+		board.set(1,2,'b');
+		board.set(1,4,'b');
+		board.set(1,6,'b');
+		
+		board.set(2,3,'b');
+		
+		board.set(7,0,'t');
+		board.set(7,2,'t');
+		board.set(7,4,'t');
+		board.set(7,6,'t');
+		
+		board.set(6,1,'t');
+		board.set(6,3,'t');
+		board.set(6,5,'t');
+		board.set(6,7,'t');
+		
+		board.set(5,4,'t');
+		
+		
+		
+		
 		Paint paint = new Paint();
 		paint.setStyle(Paint.Style.FILL);
 		paint.setARGB(255, 0, 255, 0);
@@ -37,35 +76,52 @@ public class CorrectionActivity extends Activity {
 		paint1.setARGB(255, 255, 0, 0);
 		Bitmap newImage= Bitmap.createBitmap(256, 256,Bitmap.Config.ARGB_8888); 
 		Canvas canvas= new Canvas(newImage);
-		for(int i=0; i<newImage.getWidth()-32;i+=64){
-			for(int j=0; j<newImage.getHeight()-32;j+=64){
+		for(int i=0; i<newImage.getWidth();i+=64){
+			for(int j=0; j<newImage.getHeight();j+=64){
 				canvas.drawRect(i, j, i+32, j+32, paint);
 			}
 		}
-		for(int i=32; i<newImage.getWidth()-32;i+=64){
-			for(int j=0; j<newImage.getHeight()-32;j+=64){
+		for(int i=32; i<newImage.getWidth();i+=64){
+			for(int j=0; j<newImage.getHeight();j+=64){
 				canvas.drawRect(i, j, i+32, j+32, paint1);
 			}
 		}		
-		for(int i=32; i<newImage.getWidth()-32;i+=64){
-			for(int j=32; j<newImage.getHeight()-32;j+=64){
+		for(int i=32; i<newImage.getWidth();i+=64){
+			for(int j=32; j<newImage.getHeight();j+=64){
 				canvas.drawRect(i, j, i+32, j+32, paint);
 			}
 		}
-		for(int i=0; i<newImage.getWidth()-32;i+=64){
-			for(int j=32; j<newImage.getHeight()-32;j+=64){
+		for(int i=0; i<newImage.getWidth();i+=64){
+			for(int j=32; j<newImage.getHeight();j+=64){
 				canvas.drawRect(i, j, i+32, j+32, paint1);
 			}
 		}	
+		for(int i=0; i<8;i++){
+			for(int j=0;j<8;j++){
+				if(board.get(i,j)=='b'){
+					Paint black= new Paint();
+					black.setARGB(255, 100, 100, 100);
+					black.setStyle(Paint.Style.FILL);
+					canvas.drawCircle(j*32+15, i*32+15, 10, black);
+				}
+				else if(board.get(i,j)=='t'){
+					Paint blue= new Paint();
+					blue.setARGB(255, 0, 0, 255);
+					blue.setStyle(Paint.Style.FILL);
+					canvas.drawCircle(j*32+15, i*32+15, 10, blue);
+				}
+					
+				}
+			}
 		
 		
 		ImageView mImage = (ImageView)findViewById(R.id.correctionView);
 		mImage.setImageBitmap(newImage);
 		
-	
-		
-		
 	}
+		
+		
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
