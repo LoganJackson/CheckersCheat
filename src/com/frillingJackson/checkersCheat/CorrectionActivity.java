464c2f951c0612d5a87aa6f23c26076c54194171
Logan Jackson <loganjackson95@gmail.com>
2014-04-28 18:28:30 -0400

    added orange and blue and changes accordingly

--- src/com/frillingJackson/checkersCheat/CorrectionActivity.java
+++ src/com/frillingJackson/checkersCheat/CorrectionActivity.java
@@ -37,13 +37,11 @@ public class CorrectionActivity extends Activity {
 		setContentView(R.layout.activity_correction);
 		ImageView gameboard1 = (ImageView)findViewById(R.id.correctionView);
 		registerForContextMenu(gameboard1);  
-
-		
 		
 		//O (red) = king for the dark pieces 
-		//o (black)= pawn for the dark pieces 
+		//o (blue)= pawn for the dark pieces 
 		//T (green)= king for light piece
-		//t (white)  = pawn for light pieces
+		//t (orange)  = pawn for light pieces
 		//E (empty) = empty space on board1
 		//X  = unreachable space
 		
@@ -57,99 +55,72 @@ public class CorrectionActivity extends Activity {
 //				+ "XoXoXoXo";
 		String boardString=getIntent().getStringExtra("boardStateString");
 		board1 = new GameState(boardString);
+		board1.rotateCW();
+		board1.mirror();
 		
-		//dummy gamestate
 		
-		/*
-		board1.set(1,1,'o');
-		board1.set(1,3,'o');
-		board1.set(1,5,'o');
-		board1.set(1,7,'o');
-		
-		board1.set(2,0,'o');
-		board1.set(2,2,'o');
-		board1.set(2,4,'o');
-		board1.set(,6,'o');
-		
-		board1.set(2,3,'o');
-		
-		board1.set(7,0,'t');
-		board1.set(7,2,'t');
-		board1.set(7,4,'t');
-		board1.set(7,6,'t');
-		
-		board1.set(6,1,'t');
-		board1.set(6,3,'t');
-		board1.set(6,5,'t');
-		board1.set(6,7,'t');
-		
-		board1.set(5,4,'t');
-		*/
-	
 		Paint black = new Paint();
 		black.setStyle(Paint.Style.FILL);
 		black.setARGB(255, 0, 0, 0);
 		
 		Paint red = new Paint();
 		red.setStyle(Paint.Style.FILL);
-		red.setARGB(255, 252, 1, 227);
+		red.setARGB(255, 160, 30, 5);
 		
 		Paint green = new Paint();
 		green.setStyle(Paint.Style.FILL);
-		green.setARGB(255, 0, 255, 0);
+		green.setARGB(255, 15, 120, 56);
 		
 		Paint white = new Paint();
 		white.setStyle(Paint.Style.FILL);
 		white.setARGB(255, 255, 255, 255);
 		
-		Paint brown= new Paint();
-		red.setStyle(Paint.Style.FILL);
-		red.setARGB(255,124 ,34, 34);
+		Paint blue = new Paint();
+		blue.setStyle(Paint.Style.FILL);
+		blue.setARGB(255, 10, 50, 105);
 		
-		Paint tan=new Paint();
-		tan.setStyle(Paint.Style.FILL);
-		tan.setARGB(255,185 ,126, 67);
+		Paint orange = new Paint();
+		orange.setStyle(Paint.Style.FILL);
+		orange.setARGB(255, 255, 150, 20);
 		
-		
-	Bitmap newImage= Bitmap.createBitmap(256, 256,Bitmap.Config.ARGB_8888); 
+		Bitmap newImage= Bitmap.createBitmap(256, 256,Bitmap.Config.ARGB_8888); 
 		Canvas canvas= new Canvas(newImage);
 		for(int i=0; i<newImage.getWidth();i+=64){
 			for(int j=0; j<newImage.getHeight();j+=64){
-				canvas.drawRect(i, j, i+32, j+32, tan);
+				canvas.drawRect(i, j, i+32, j+32, black);
 			}
 		}
 		for(int i=32; i<newImage.getWidth();i+=64){
 			for(int j=0; j<newImage.getHeight();j+=64){
-				canvas.drawRect(i, j, i+32, j+32, brown);
+				canvas.drawRect(i, j, i+32, j+32, white);
 			}
 		}		
 		for(int i=32; i<newImage.getWidth();i+=64){
 			for(int j=32; j<newImage.getHeight();j+=64){
-				canvas.drawRect(i, j, i+32, j+32, tan);
+				canvas.drawRect(i, j, i+32, j+32, black);
 			}
 		}
 		for(int i=0; i<newImage.getWidth();i+=64){
 			for(int j=32; j<newImage.getHeight();j+=64){
-				canvas.drawRect(i, j, i+32, j+32, brown);
+				canvas.drawRect(i, j, i+32, j+32, white);
 			}
 		}	
 		for(int i=0; i<8;i++){
 			for(int j=0;j<8;j++){
 				if(board1.get(i,j)=='o'){
-					canvas.drawCircle(j*32+15, i*32+15, 10, black);
+					canvas.drawCircle(j*32+15, i*32+15, 10, blue);
 				}
 				else if(board1.get(i,j)=='O'){
 					canvas.drawCircle(j*32+15, i*32+15, 10, red);
 				}
 				else if(board1.get(i, j)=='t'){
-					canvas.drawCircle(j*32+15, i*32+15, 10,white);
+					canvas.drawCircle(j*32+15, i*32+15, 10,orange);
 				}
 				else if(board1.get(i, j)=='T'){
 					canvas.drawCircle(j*32+15, i*32+15, 10,green);
 				}
 			}
 		}
-		
 		
 		String stringBoard="";
 		for (int i=0; i<=7;i++){
@@ -172,41 +143,34 @@ public class CorrectionActivity extends Activity {
 	                return true;      
 	            }
 	    });
-		
-		
 		gameboard1.setImageBitmap(newImage);
-
 	}
 	
-	 private void showPopupMenu(View v){
+	 private void showPopupMenu(View v){ 
+		 PopupMenu popupMenu = new PopupMenu(CorrectionActivity.this, v);
+		 popupMenu.getMenuInflater().inflate(R.menu.popupmenu, popupMenu.getMenu());
 		 
-		 
-		   PopupMenu popupMenu = new PopupMenu(CorrectionActivity.this, v);
-		      popupMenu.getMenuInflater().inflate(R.menu.popupmenu, popupMenu.getMenu());
-		      
-		      popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-		   
+		 popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+			 
 		   @Override
-		  public boolean onMenuItemClick(MenuItem item) {
-			GameState board1 = new GameState();
-		   int itemId = item.getItemId();
-		if (itemId == R.id.Red) {
-			board1=correctboard1(x,y, 'O');
-		} else if (itemId == R.id.Black) {
-			board1=correctboard1(x,y,'o');
-		} else if (itemId == R.id.Green) {
-			board1=correctboard1(x,y,'T');
-		} else if (itemId == R.id.White) {
-			board1=correctboard1(x,y,'t');
-		}
-		else if (itemId == R.id.Remove){
-			board1=correctboard1(x,y,'X');
-		}
-		    return true;
+		   public boolean onMenuItemClick(MenuItem item) {
+			//GameState board1 = new GameState();
+			   int itemId = item.getItemId();
+			   if (itemId == R.id.Red) {
+				   board1=correctboard1(x,y, 'O');
+			   } else if (itemId == R.id.Blue) {
+				   board1=correctboard1(x,y,'o');
+			   } else if (itemId == R.id.Green) {
+				   board1=correctboard1(x,y,'T');
+			   } else if (itemId == R.id.Orange) {
+				   board1=correctboard1(x,y,'t');
+			   } else if (itemId == R.id.Remove){
+				   board1=correctboard1(x,y,'E');
+			   }
+			   return true;
 		   }
-		  });
-		      
-		      popupMenu.show();
+		});
+		 popupMenu.show();
 	 }
 
 	@Override
@@ -216,28 +180,20 @@ public class CorrectionActivity extends Activity {
 		return true;
 	}
 	
-	
-	
 	public void sendBoard1(View view){
 		String stringBoard="";
-		
 		for (int i=0; i<=7;i++){
 			for(int j=0;j<=7;j++){
 				stringBoard=stringBoard+board1.get(i,j);
 			}
 		}
-		
 		//GameState newboard11 = new GameState(newboard1.board1);
 		Intent intent = new Intent(this, Suggestion.class);
 		intent.putExtra("corrected",stringBoard);
 		startActivity(intent);
 	}
-	
-
 
 	GameState correctboard1( int xCoordinate, int yCoordinate, char ch){
-		
-		
 		
 		Paint black = new Paint();
 		black.setStyle(Paint.Style.FILL);
@@ -245,25 +201,23 @@ public class CorrectionActivity extends Activity {
 		
 		Paint red = new Paint();
 		red.setStyle(Paint.Style.FILL);
-		red.setARGB(255, 252, 1, 227);
+		red.setARGB(255, 160, 30, 5);
 		
 		Paint green = new Paint();
 		green.setStyle(Paint.Style.FILL);
-		green.setARGB(255, 0, 255, 0);
+		green.setARGB(255, 15, 120, 56);
 		
 		Paint white = new Paint();
 		white.setStyle(Paint.Style.FILL);
 		white.setARGB(255, 255, 255, 255);
 		
-		Paint brown= new Paint();
-		red.setStyle(Paint.Style.FILL);
-		red.setARGB(255,124 ,34, 34);
+		Paint blue = new Paint();
+		blue.setStyle(Paint.Style.FILL);
+		blue.setARGB(255, 10, 50, 105);
 		
-		Paint tan=new Paint();
-		tan.setStyle(Paint.Style.FILL);
-		tan.setARGB(255,185 ,126, 67);
-		
-		
+		Paint orange = new Paint();
+		orange.setStyle(Paint.Style.FILL);
+		orange.setARGB(255, 255, 150, 20);
 		
 		board1.set(yCoordinate, xCoordinate, ch);
 		Bitmap newImage1= Bitmap.createBitmap(256, 256,Bitmap.Config.ARGB_8888); 
@@ -271,40 +225,38 @@ public class CorrectionActivity extends Activity {
 		
 		for(int i=0; i<newImage1.getWidth();i+=64){
 			for(int j=0; j<newImage1.getHeight();j+=64){
-				canvas1.drawRect(i, j, i+32, j+32, tan);
+				canvas1.drawRect(i, j, i+32, j+32, black);
 			}
 		}
 		for(int i=32; i<newImage1.getWidth();i+=64){
 			for(int j=0; j<newImage1.getHeight();j+=64){
-				canvas1.drawRect(i, j, i+32, j+32, brown);
+				canvas1.drawRect(i, j, i+32, j+32, white);
 			}
 		}		
 		for(int i=32; i<newImage1.getWidth();i+=64){
 			for(int j=32; j<newImage1.getHeight();j+=64){
-				canvas1.drawRect(i, j, i+32, j+32, tan);
+				canvas1.drawRect(i, j, i+32, j+32, black);
 			}
 		}
 		for(int i=0; i<newImage1.getWidth();i+=64){
 			for(int j=32; j<newImage1.getHeight();j+=64){
-				canvas1.drawRect(i, j, i+32, j+32, brown);
+				canvas1.drawRect(i, j, i+32, j+32, white);
 			}
 		}	
 		for(int i=0; i<8;i++){
 			for(int j=0;j<8;j++){
 				if(board1.get(i,j)=='o'){
-					canvas1.drawCircle(j*32+15, i*32+15, 10, black);
+					canvas1.drawCircle(j*32+15, i*32+15, 10, blue);
 				}
 				else if(board1.get(i,j)=='O'){
 					canvas1.drawCircle(j*32+15, i*32+15, 10, red);
 				}
 				else if(board1.get(i, j)=='t'){
-					canvas1.drawCircle(j*32+15, i*32+15, 10, white);
+					canvas1.drawCircle(j*32+15, i*32+15, 10, orange);
 				}
 				else if(board1.get(i, j)=='T'){
 					canvas1.drawCircle(j*32+15, i*32+15, 10,green);
 				}
-				
-				
 			}
 		}
 		
--- src/com/frillingJackson/checkersCheat/Suggestion.java
+++ src/com/frillingJackson/checkersCheat/Suggestion.java
@@ -11,6 +11,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ImageView;
 
 public class Suggestion extends Activity {
@@ -22,95 +23,83 @@ public class Suggestion extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_suggestion);
 		
-		
 		boardString=getIntent().getStringExtra("corrected");
-		  board1= new GameState(boardString);
-		  
-		  
-		  Paint black = new Paint();
-			black.setStyle(Paint.Style.FILL);
-			black.setARGB(255, 0, 0, 0);
-			
-			Paint red = new Paint();
-			red.setStyle(Paint.Style.FILL);
-			red.setARGB(255, 252, 1, 227);
-			
-			Paint green = new Paint();
-			green.setStyle(Paint.Style.FILL);
-			green.setARGB(255, 0, 255, 0);
-			
-			Paint white = new Paint();
-			white.setStyle(Paint.Style.FILL);
-			white.setARGB(255, 255, 255, 255);
-			
-			Paint brown= new Paint();
-			red.setStyle(Paint.Style.FILL);
-			red.setARGB(255,124 ,34, 34);
-			
-			Paint tan=new Paint();
-			tan.setStyle(Paint.Style.FILL);
-			tan.setARGB(255,185 ,126, 67);
-			
-			Paint yellow=new Paint();
-			yellow.setStyle(Paint.Style.STROKE);
-			yellow.setARGB(255, 233, 247, 7);
-			
-			
-			
-			
+		board1= new GameState(boardString);
+		    
+		Paint black = new Paint();
+		black.setStyle(Paint.Style.FILL);
+		black.setARGB(255, 0, 0, 0);
+		
+		Paint red = new Paint();
+		red.setStyle(Paint.Style.FILL);
+		red.setARGB(255, 160, 30, 5);
+		
+		Paint green = new Paint();
+		green.setStyle(Paint.Style.FILL);
+		green.setARGB(255, 15, 120, 56);
+		
+		Paint white = new Paint();
+		white.setStyle(Paint.Style.FILL);
+		white.setARGB(255, 255, 255, 255);
+		
+		Paint blue = new Paint();
+		blue.setStyle(Paint.Style.FILL);
+		blue.setARGB(255, 10, 50, 105);
+		
+		Paint orange = new Paint();
+		orange.setStyle(Paint.Style.FILL);
+		orange.setARGB(255, 255, 150, 20);
+		
+		Paint yellow=new Paint();
+		yellow.setStyle(Paint.Style.STROKE);
+		yellow.setARGB(255, 255, 0, 255);		
+		
 		Bitmap newImage= Bitmap.createBitmap(256, 256,Bitmap.Config.ARGB_8888); 
-			Canvas canvas= new Canvas(newImage);
-			for(int i=0; i<newImage.getWidth();i+=64){
-				for(int j=0; j<newImage.getHeight();j+=64){
-					canvas.drawRect(i, j, i+32, j+32, tan);
+		Canvas canvas= new Canvas(newImage);
+		for(int i=0; i<newImage.getWidth();i+=64){
+			for(int j=0; j<newImage.getHeight();j+=64){
+				canvas.drawRect(i, j, i+32, j+32, black);
+			}
+		}
+		for(int i=32; i<newImage.getWidth();i+=64){
+			for(int j=0; j<newImage.getHeight();j+=64){
+				canvas.drawRect(i, j, i+32, j+32, white);
+			}
+		}		
+		for(int i=32; i<newImage.getWidth();i+=64){
+			for(int j=32; j<newImage.getHeight();j+=64){
+				canvas.drawRect(i, j, i+32, j+32, black);
+			}
+		}
+		for(int i=0; i<newImage.getWidth();i+=64){
+			for(int j=32; j<newImage.getHeight();j+=64){
+				canvas.drawRect(i, j, i+32, j+32, white);
+			}
+		}	
+		for(int i=0; i<8;i++){
+			for(int j=0;j<8;j++){
+				if(board1.get(i,j)=='o'){
+					canvas.drawCircle(j*32+15, i*32+15, 10, blue);
+				}
+				else if(board1.get(i,j)=='O'){
+					canvas.drawCircle(j*32+15, i*32+15, 10, red);
+				}
+				else if(board1.get(i, j)=='t'){
+					canvas.drawCircle(j*32+15, i*32+15, 10, orange);
+				}
+				else if(board1.get(i, j)=='T'){
+					canvas.drawCircle(j*32+15, i*32+15, 10,green);
 				}
 			}
-			for(int i=32; i<newImage.getWidth();i+=64){
-				for(int j=0; j<newImage.getHeight();j+=64){
-					canvas.drawRect(i, j, i+32, j+32, brown);
-				}
-			}		
-			for(int i=32; i<newImage.getWidth();i+=64){
-				for(int j=32; j<newImage.getHeight();j+=64){
-					canvas.drawRect(i, j, i+32, j+32, tan);
-				}
-			}
-			for(int i=0; i<newImage.getWidth();i+=64){
-				for(int j=32; j<newImage.getHeight();j+=64){
-					canvas.drawRect(i, j, i+32, j+32, brown);
-				}
-			}	
-			for(int i=0; i<8;i++){
-				for(int j=0;j<8;j++){
-					if(board1.get(i,j)=='o'){
-						canvas.drawCircle(j*32+15, i*32+15, 10, black);
-					}
-					else if(board1.get(i,j)=='O'){
-						canvas.drawCircle(j*32+15, i*32+15, 10, red);
-					}
-					else if(board1.get(i, j)=='t'){
-						canvas.drawCircle(j*32+15, i*32+15, 10,white );
-					}
-					else if(board1.get(i, j)=='T'){
-						canvas.drawCircle(j*32+15, i*32+15, 10,green);
-					}
-				}
-			}
-			ImageView gameboard1=  (ImageView)findViewById(R.id.SuggestImage);
-			gameboard1.setImageBitmap(newImage);
-			
-			
-			
-			bestMove=findBestMove();
-			canvas.drawRect(bestMove.from().col*32, bestMove.from().row*32, bestMove.from().col*32+32, bestMove.from().row*32+32,yellow );
-			canvas.drawRect(bestMove.to().col*32, bestMove.to().row*32,bestMove.to().col *32+32,bestMove.to().row*32+32,yellow);
-			Log.d("bestMove", bestMove.toString());
-			
-			
-	}
-	
-		
-		
+		}
+		ImageView gameboard1=  (ImageView)findViewById(R.id.SuggestImage);
+		gameboard1.setImageBitmap(newImage);
+					
+		bestMove=findBestMove();
+		canvas.drawRect(bestMove.from().col*32, bestMove.from().row*32, bestMove.from().col*32+32, bestMove.from().row*32+32,yellow );
+		canvas.drawRect(bestMove.to().col*32, bestMove.to().row*32,bestMove.to().col *32+32,bestMove.to().row*32+32,yellow);
+		Log.d("bestMove", bestMove.toString());	
+	}	
 	
 @Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -119,22 +108,21 @@ public class Suggestion extends Activity {
 		return true;
 	}
 
-	public Move findBestMove(){
-		
-	
-	CheckersAI checkersAI=CheckersAI.getInstance();
-	
-	Move bestMove = null;
-	try {
-		bestMove = checkersAI.getBestMove(board1,4);
-		Log.d("bestMove", bestMove.toString());
-		return bestMove;
-		
-	} catch (NoMovesLeftException e) {
-		
-		e.printStackTrace();
-	}
-	return bestMove;
+	public void goHome(View view){
+		Intent intent = new Intent(this, Home.class);
+		startActivity(intent);
 	}
 
+	public Move findBestMove(){
+		CheckersAI checkersAI=CheckersAI.getInstance();
+		Move bestMove = null;
+		try {
+			bestMove = checkersAI.getBestMove(board1,3);
+			Log.d("bestMove", bestMove.toString());
+			return bestMove;		
+		} catch (NoMovesLeftException e) {	
+			e.printStackTrace();
+		}
+		return bestMove;
+	}
 }
